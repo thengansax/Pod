fd187f1c9c42728a576a73ee1c24f994cfc98ded
thengansax <thengansax612@gmail.com>
2022-04-21 16:43:28 +0700

    d

---
+++ app/scripts/about.js
@@ -0,0 +1,8 @@
+$('.features_slider').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    prevArrow: '<div class="slick-prev"><i class="fa-solid fa-angle-right"></i></div>',
+    nextArrow: '<div class="slick-next"><i class="fa-solid fa-chevron-left"></i></div>',
+    centerMode: true,
+  });
