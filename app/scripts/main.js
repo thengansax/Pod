cc01bb16c29cd7bb5f2be93e74a8b4db1cad796e
thengansax <thengansax612@gmail.com>
2022-04-21 09:30:35 +0700

    update

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,25 @@
-console.log('\'Allo \'Allo!');
+$('.content2').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    prevArrow:false,
+    nextArrow:false,
+    // centerMode: true,
+  });
 
-// Uncomment to enable Bootstrap tooltips
-// https://getbootstrap.com/docs/4.0/components/tooltips/#example-enable-tooltips-everywhere
-// $(function () { $('[data-toggle="tooltip"]').tooltip(); });
-
-// Uncomment to enable Bootstrap popovers
-// https://getbootstrap.com/docs/4.0/components/popovers/#example-enable-popovers-everywhere
-// $(function () { $('[data-toggle="popover"]').popover(); });
+  // $('.features_slider').slick({
+  //   dots: true,
+  //   infinite: true,
+  //   speed: 300,
+  //   slidesToShow: 2,
+  //   centerMode: true,
+  //   variableWidth: true
+  // });
+  $('.features_slider').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    prevArrow: '<div class="slick-prev"><i class="fa-solid fa-angle-right"></i></div>',
+    nextArrow: '<div class="slick-next"><i class="fa-solid fa-chevron-left"></i></div>',
+    centerMode: true,
+  });
